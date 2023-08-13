06fa73fbdc4cdd6fea04fc750f7d76edae8acc43
Geinersb <geinersb20@gmail.com>
2023-08-12 19:48:18 -0600

    se ingresa  proyecto

---
+++ android/app/src/main/kotlin/com/example/tok_tik_datasources/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tok_tik_datasources
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/widgets/shared/video_scroll_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:tok_tik_datasources/domain/entities/video_post.dart';
+import 'package:tok_tik_datasources/presentation/widgets/shared/video_buttons.dart';
+import 'package:tok_tik_datasources/presentation/widgets/video/fullscreen_player.dart';
+
+
+class VidelScrollView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  const VidelScrollView({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      physics: const BouncingScrollPhysics(),
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+//video +gradiente
+SizedBox.expand(
+  child: FullScreenPlayer(
+caption: videoPost.name,
+videoUrl: videoPost.videoUrl,
+
+
+  )
+),
+
+
+
+//botones
+            Positioned(
+                bottom: 40, right: 20, child: VideoButtons(video: videoPost)),
+          ],
+        );
+      },
+    );
+  }
+}
+
